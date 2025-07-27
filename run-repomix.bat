841fa89687d0d78920277a19b3c59067244e5d5a
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2025-07-27 14:15:55 +0300

    הוספה: יצירת repomix

---
+++ run-repomix.bat
@@ -0,0 +1,5 @@
+@echo off
+
+repomix --style markdown -i "otzaria-library/**,gmach-otzar-hatora/output_dataset/**,gmach-otzar-hatora/converted_parquet/**,alonim_dataset/**,*.jsonl,*.parquet,source_data/**"
+
+pause
